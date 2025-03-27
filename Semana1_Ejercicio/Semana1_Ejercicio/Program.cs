6cae23d1bac9983826ed8f3602f1266ae5efda5f
Haruo <haruohwr@gmail.com>
2025-03-26 20:57:56 -0500

    Constructor?

--- Semana1_Ejercicio/Semana1_Ejercicio/Program.cs
+++ Semana1_Ejercicio/Semana1_Ejercicio/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Personaje
+{
+    protected string Nombre;
+
+    public Personaje(string nombre)
+    {
+        Nombre = nombre;
+    }
+}
