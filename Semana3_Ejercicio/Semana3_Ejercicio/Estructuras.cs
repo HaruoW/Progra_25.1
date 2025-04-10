a8f211fa83982ae2e900f3f72e3aabe8546b9813
Haruo <haruohwr@gmail.com>
2025-04-09 21:52:32 -0500

    avance

---
+++ Semana3_Ejercicio/Semana3_Ejercicio/Estructuras.cs
@@ -0,0 +1,45 @@
+﻿namespace Estructuras
+{
+    public abstract class Estructura
+    {
+        public string Nombre;
+        public int TurnosRestantes;
+        public int Costo;
+        public bool Construida => TurnosRestantes <= 0;
+    }
+
+    public class Granja : Estructura
+    {
+        public Granja()
+        {
+            Nombre = "Granja";
+            TurnosRestantes = 5;
+            Costo = 50;
+        }
+    }
+
+    public class Torre : Estructura
+    {
+        public int Vida = 100;
+        public int Daño = 20;
+
+        public Torre()
+        {
+            Nombre = "Torre";
+            TurnosRestantes = 10;
+            Costo = 100;
+        }
+    }
+
+    public class Casa : Estructura
+    {
+        public bool Ocupada = false;
+
+        public Casa()
+        {
+            Nombre = "Casa";
+            TurnosRestantes = 2;
+            Costo = 50;
+        }
+    }
+}
