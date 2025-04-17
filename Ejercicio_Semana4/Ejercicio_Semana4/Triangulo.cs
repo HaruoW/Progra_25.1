8b92c4be7716138ccc5b5f858ab8dc084e7efb63
Haruo <haruohwr@gmail.com>
2025-04-16 21:01:25 -0500

    Triangulo

---
+++ Ejercicio_Semana4/Ejercicio_Semana4/Triangulo.cs
@@ -0,0 +1,17 @@
+﻿public class Triangulo : Figura
+{
+    public double Base { get; set; }
+    public double Altura { get; set; }
+
+    public Triangulo(double @base, double altura)
+        : base("Triángulo")
+    {
+        Base = @base;
+        Altura = altura;
+    }
+
+    public override double CalcularArea()
+    {
+        return (Base * Altura) / 2;
+    }
+}
