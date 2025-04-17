190796a08b4fbfe342abb147c65b9e5cc0bf5abb
Haruo <haruohwr@gmail.com>
2025-04-16 20:57:03 -0500

    Cuadrado 2

---
+++ Ejercicio_Semana4/Ejercicio_Semana4/Cuadrado.cs
@@ -0,0 +1,16 @@
+﻿public class Cuadrado : Rectangulo
+{
+    public double Lado { get; set; }
+
+    public Cuadrado(double lado)
+        : base(lado, lado)
+    {
+        Lado = lado;
+        Nombre = "Cuadrado";
+    }
+
+    public override double CalcularArea()
+    {
+        return Lado * Lado;
+    }
+}
